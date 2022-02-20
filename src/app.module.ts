b1cd1657cb8a2ef6b1052429aaed3c562672d43b
Jeayoeb Kim <jykim@atones.co.kr>
2022-02-20 23:42:57 +0900

    nest configure 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigService],
 })
 export class AppModule {}
