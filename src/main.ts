42f03c071ea09de2685b3de33c6a7eda13c2a1a5
Jeayoeb Kim <jykim@atones.co.kr>
2022-03-03 23:11:09 +0900

    main.ts 수정 cors, validation pipe, cookie parser, passport

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {HttpExceptionFilter} from "./http-exception.filter";
+import { HttpExceptionFilter } from "./http-exception.filter";
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import passport from "passport";
+import session from 'express-session';
 
 declare const module: any;
 
@@ -12,6 +15,15 @@ async function bootstrap() {
 
   // HttpExceptionFilter 적용
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Validation Pipe
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+
+  // CORS enable
+  app.enableCors({
+    origin: true,
+    credentials: true
+  });
 
   // Swagger
   const config = new DocumentBuilder()
@@ -26,6 +38,20 @@ async function bootstrap() {
 
   // Logger - Pino
   app.useLogger(app.get(Logger));
+
+  app.use(cookieParser());
+  app.use(
+      session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+          htppOnly: true
+        }
+      })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
